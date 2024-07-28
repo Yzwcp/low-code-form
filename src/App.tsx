9eb388d37df008faf51d714d5f558cb0baed413a
umep <1774570823@qq.com>
2024-07-28 18:33:41 +0800

    feat: 添加commitlint

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-function App() {s
-  let s = s'1's
+function App() {
   const [count, setCount] = useState(0)
   return (
     <>
