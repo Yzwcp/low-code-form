0f5aab66806d264ff3433bf6d46260a16458e57e
yzw <1774570823@qq.com>
2024-07-29 16:42:42 +0800

    chore: scss保存格式化

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,4 +1,9 @@
+import styles from './QuestionCard.module.scss'
 function QuestionCard() {
-  return <></>
+  return (
+    <>
+      <div className={styles.index}>111111111</div>
+    </>
+  )
 }
 export default QuestionCard
--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,6 +1,11 @@
 import React, { FC } from 'react'
+import QuestionCard from '../components/QuestionCard.tsx'
 const List: FC = () => {
-  return <>List FE</>
+  return (
+    <>
+      <QuestionCard></QuestionCard>
+    </>
+  )
 }
 
 export default List
